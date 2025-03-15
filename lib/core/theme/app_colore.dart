b4dfd704580787566da2a3cdaef92a8676d25997
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-15 14:27:04 +0200

    crate home sceen

--- lib/core/theme/app_colore.dart
+++ lib/core/theme/app_colore.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 class AppColore {
   static const Color ScaffoldBGColor = Color(0xff13140D);
+  static const Color KwitheTExt = Color(0xffFFFFFF);
+  static const Color KgreyTExt = Color(0xff858585);
+  static const Color KListButton = Color(0xff30312D);
+  static const Color KhomeButton = Color(0xffBEDE61);
+  static const Color KhomeButtongry = Color(0xff787878);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/home_screen.dart';
+import 'package:news_app/featured/home/presentation/widget/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
+          textTheme: TextTheme(),
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
