a63a9ad7bbad3d30bbaf451bc967bb1c5f44f2fb
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-16 14:41:58 +0200

    create errors message

--- lib/logic/character_cubit/character_cubit.dart
+++ lib/logic/character_cubit/character_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+// ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
+import 'package:news_app/data/model/character/character.dart';
 
 part 'character_state.dart';
 
--- lib/logic/character_cubit/character_state.dart
+++ lib/logic/character_cubit/character_state.dart
@@ -4,3 +4,17 @@ part of 'character_cubit.dart';
 sealed class CharacterState {}
 
 final class CharacterInitial extends CharacterState {}
+
+final class CharacterSuccess extends CharacterState {
+  final List<Character> characters;
+
+  CharacterSuccess({required this.characters});
+}
+
+final class CharacterLoading extends CharacterState {}
+
+final class CharacterFaulier extends CharacterState {
+  final String errMessage;
+
+  CharacterFaulier({required this.errMessage});
+}
